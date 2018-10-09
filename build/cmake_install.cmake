b0e9cd0d155d26fc22899643d7d37c0253086320
H2O-2 <h2o-2@qq.com>
2018-10-09 13:42:50 +0900

    configs

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/peiranhong/Projects/cmakeTest
+# Install script for directory: /Users/ymmtlab/peiran/learnopengl
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/peiranhong/Projects/cmakeTest/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/ymmtlab/peiran/learnopengl/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
