a03a7759b82217d39705adf86ded50d5fd538030
H2O-2 <h2o-2@qq.com>
2018-10-22 16:39:27 +0900

    Mesh

---
+++ src/mesh.cpp
@@ -0,0 +1,59 @@
+#include "mesh.h"
+
+Mesh::Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> texture) {
+    this->vertices = vertices;
+    this->indices = indices;
+    this->texture = texture;
+
+    setupMesh();
+}
+
+void Mesh::Draw(Shader shader) {
+    unsigned int diffuseNr = 1;
+    unsigned int specularNr = 1;
+
+    for (unsigned int i = 0; i < texture.size(); i++) {
+        glActiveTexture(GL_TEXTURE0 + i);
+        string number;
+        string name = texture[i].type;
+        if (name == DIFFUSE) {
+            number = to_string(diffuseNr++);
+        } else if (name == SPECULAR) {
+            number = to_string(specularNr++);
+        }
+
+        shader.setFloat(("material." + name + number).c_str(), i);
+        glBindTexture(GL_TEXTURE_2D, texture[i].id);
+    }
+    glActiveTexture(GL_TEXTURE0);
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
+
+void Mesh::setupMesh() {
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+    // Vertices
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+    // Normals
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+    // Texture
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoord));
+
+    glBindVertexArray(0);
+}
---
+++ src/mesh.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <iostream>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <string>
+#include <vector>
+
+#include "shader_s.h"
+
+using namespace std;
+using namespace glm;
+
+struct Vertex {
+    vec3 Position;
+    vec3 Normal;
+    vec3 TexCoord;
+};
+
+struct Texture {
+    unsigned int id;
+    string type;
+};
+
+class Mesh {
+public:
+    vector<Vertex> vertices;
+    vector<unsigned int> indices;
+    vector<Texture> texture;
+
+    Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> texture);
+    void Draw(Shader shader);
+private:
+    unsigned VAO, VBO, EBO;
+    const string DIFFUSE = "texture_diffuse";
+    const string SPECULAR = "texture_specular";
+
+    void setupMesh();
+};
